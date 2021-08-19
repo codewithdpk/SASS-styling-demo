123b7da1e1636b4d8bf3e949361e17a78ddcc71c
Deepak Suthar <60180495+codewithdpk@users.noreply.github.com>
2021-08-19 13:25:12 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# SASS-styling-demo
-Created with CodeSandbox
+# SASS-styling-demo ✨
+
+This is a simple project to understand basics of `sass-lang` (Sassy CSS) where I have practiced and understood cors of SASS such as creating variables, using `@mixing`, `@includes`, and nesting.
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./scss/style.scss";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <button className="btn-primary">Click here</button>
+      <button className="btn-success">Click here</button>
+    </div>
+  );
+}
